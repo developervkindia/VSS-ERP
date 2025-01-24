5d49b8496dd117d63cb680cf24f7f0050befdc28
Rohit Panchal <panchal.97rohit@gmail.com>
2025-01-24 10:22:30 +0530

    Project CRUD added

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Project extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'user_id', 'start_date', 'end_date', 'description', 'type'];
+
+    public function setStartDateAttribute($value)
+    {
+        $this->attributes['start_date'] = $value ? Carbon::parse($value) : NULL;
+    }
+
+    public function setEndDateAttribute($value)
+    {
+        $this->attributes['end_date'] = $value ? Carbon::parse($value) : NULL;
+    }
+
+    public function getStartDateAttribute($value)
+    {
+        return $value ? Carbon::parse($value)->format('d-m-Y') : NULL;
+    }
+
+    public function getEndDateAttribute($value)
+    {
+        return $value ? Carbon::parse($value)->format('d-m-Y') : NULL;
+    }
 }
--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -18,8 +18,11 @@
         <div class="col-sm-12">
             <div class="card">
                 <div class="card-body">
-                    <form action="{{ route('projects.store') }}" method="POST">
+                    <form action="{{ isset($project) ? route('projects.update', $project->id) : route('projects.store') }}" method="POST">
                         @csrf
+                        @isset($project)
+                            @method('PUT')
+                        @endisset
                         <div class="row">
                             <div class="col-12">
                                 <h5 class="form-title"><span>Project Information</span></h5>
@@ -27,7 +30,7 @@
                             <div class="col-12 col-sm-6">
                                 <div class="form-group">
                                     <label>Project Title</label>
-                                    <input type="text" name="title" class="form-control @error('title') is-invalid @enderror""/>
+                                    <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" placeholder="Enter project title" value="{{ old('title', $project->title ?? '') }}" />
                                     @error('title')
                                         <span class="text-danger text-sm">{{ $message }}</span>
                                     @enderror
@@ -39,34 +42,46 @@
                                     @php
                                         $projecTypes = ['public', 'private'];
                                     @endphp
-                                    <select class="form-control select" name="type" id="exampleFormControlSelect1">
+                                    <select class="form-control select @error('type') is-invalid @enderror" name="type" id="exampleFormControlSelect1">
                                         <option selected disabled>Select Project</option>
                                         @foreach ($projecTypes as $projecType)
-                                            <option value="{{ $projecType }}">{{ ucwords($projecType) }}</option>
+                                            <option value="{{ $projecType }}" {{ old('type', $project->type ?? '') ? 'selected' : '' }}>{{ ucwords($projecType) }}</option>
                                         @endforeach
                                     </select>
+                                    @error('type')
+                                        <small class="text-danger">{{ $message }}</small>
+                                    @enderror
                                 </div>
                             </div>
-                            <div class="col-12 col-sm-6">
+                            <div class="col-12 col-md-6">
                                 <div class="form-group">
-                                    <label>Start Date</label>
-                                    <input type="date" class="form-control" name="start_date" value="{{ old('start_date') }}" />
+                                    <label for="start_date">Start Date</label>
+                                    <input type="text" id="start_date" class="form-control datetimepicker @error('start_date') is-invalid @enderror" name="start_date" value="{{ old('start_date', $project->start_date ?? '') }}" />
+                                    @error('start_date')
+                                        <small class="text-danger">{{ $message }}</small>
+                                    @enderror
                                 </div>
                             </div>
-                            <div class="col-12 col-sm-6">
+                            <div class="col-12 col-md-6">
                                 <div class="form-group">
-                                    <label>End Date</label>
-                                    <input type="date" class="form-control" name="end_date" value="{{ old('end_date') }}" />
+                                    <label for="end_date">End Date</label>
+                                    <input type="text" id="end_date" class="form-control datetimepicker @error('end_date') is-invalid @enderror" name="end_date" value="{{ old('end_date', $project->end_date ?? '') }}" />
+                                    @error('end_date')
+                                        <small class="text-danger">{{ $message }}</small>
+                                    @enderror
                                 </div>
                             </div>
                             <div class="col-lg-12 col-md-12">
                                 <div class="form-group">
                                     <label>Description</label>
-                                    <textarea name="description" id="editor" cols="30" rows="10">{{ old('description') }}</textarea>
+                                    <textarea name="description" id="editor" cols="30" rows="10">{{ old('description', $project->description ?? '') }}</textarea>
+                                    @error('description')
+                                        <small class="text-danger">{{ $message }}</small>
+                                    @enderror
                                 </div>
                             </div>
                             <div class="col-12">
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="submit" class="btn btn-primary">{{ isset($project) ? 'Save' : 'Submit' }}</button>
                             </div>
                         </div>
                     </form>
